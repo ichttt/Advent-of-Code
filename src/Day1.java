eefb9a7aac5a160c64d4d11fe70f13ca376ac6b6
Tobias Hotz <tobias.hotz@hotmail.de>
2016-12-02 15:44:35 +0100

    Minor simplification

--- src/Day1.java
+++ src/Day1.java
@@ -55,18 +55,18 @@ public class Day1 {
     private static final String path = "R4, R1, L2, R1, L1, L1, R1, L5, R1, R5, L2, R3, L3, L4, R4, R4, R3, L5, L1, R5, R3, L4, R1, R5, L1, R3, L2, R3, R1, L4, L1, R1, L1, L5, R1, L2, R2, L3, L5, R1, R5, L1, R188, L3, R2, R52, R5, L3, R79, L1, R5, R186, R2, R1, L3, L5, L2, R2, R4, R5, R5, L5, L4, R5, R3, L4, R4, L4, L4, R5, L4, L3, L1, L4, R1, R2, L5, R3, L4, R3, L3, L5, R1, R1, L3, R2, R1, R2, R2, L4, R5, R1, R3, R2, L2, L2, L1, R2, L1, L3, R5, R1, R4, R5, R2, R2, R4, R4, R1, L3, R4, L2, R2, R1, R3, L5, R5, R2, R5, L1, R2, R4, L1, R5, L3, L3, R1, L4, R2, L2, R1, L1, R4, R3, L2, L3, R3, L2, R1, L4, R5, L1, R5, L2, L1, L5, L2, L5, L2, L4, L2, R3";
     public static void main(String args[]) {
         String[] split = path.split(",");
-        for(int i = 0; i<split.length;i++) {
-            if(split[i].startsWith(" "))
-                split[i] = split[i].substring(1);
+        for(String s:split) {
+            if(s.startsWith(" "))
+                s = s.substring(1);
 
-            assert split[i].length() >=2;
-            if(split[i].startsWith("R"))
+            assert s.length() >=2;
+            if(s.startsWith("R"))
                 turnRight();
-            else if(split[i].startsWith("L"))
+            else if(s.startsWith("L"))
                 turnLeft();
             else
-                throw new IllegalArgumentException(split[i]);
-            walk(Integer.parseInt(split[i].substring(1)));
+                throw new IllegalArgumentException(s);
+            walk(Integer.parseInt(s.substring(1)));
         }
         
         System.out.println(posX + posY);
